5fb2a5e9c7f01533c9c2ded2231c11484e50bcb1
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-09-23 23:23:18 -0300

    agrego atributo listado de pedidos a clase cadete

--- Cadeteria/Cadete.cs
+++ Cadeteria/Cadete.cs
@@ -2,6 +2,15 @@ namespace Practico3
 {
     public class Cadete : Persona
     {
-        
+        list<Pedidos> listadoPedidosCadete;
+
+        public list<Pedidos> ListadoPedidosCadete { get => listadoPedidosCadete; set => listadoPedidosCadete = value; }
+
+        public Cadete () {}
+
+        public Cadete(list<Pedidos> listadoPedidosCadete)
+        {
+            ListadoPedidosCadete = listadoPedidosCadete;
+        }
     }
 }
